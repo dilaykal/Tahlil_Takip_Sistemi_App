1c017ee5b7b04319da0a952d660a2de1e04f9d1b
Dilay Kal <104075669+dilaykal@users.noreply.github.com>
2024-12-28 17:35:06 +0300

    Stil ayarlamaları kolaylaştırıldı

---
+++ component/CustomTextInput.js
@@ -0,0 +1,41 @@
+import { StyleSheet, Text, View , TextInput} from "react-native";
+import React from "react";
+
+const CustomTextInput = ({title,isSecureText,handleOnChangeText,handleValue,handlePlaceholder}) =>{
+    return(
+        <View style={styles.inputContainer}>
+            <Text style={styles.inputBoxText} >{title}</Text>
+            <TextInput 
+                placeholder={handlePlaceholder} 
+                secureTextEntry={isSecureText}
+                style={styles.textInputStyle} 
+                keyboardType='numeric' 
+                onChangeText={(text) => handleOnChangeText(text)}
+                value={handleValue}
+            />
+        </View>
+    )
+}
+ export default CustomTextInput
+ const styles = StyleSheet.create({
+    inputContainer:{
+        width:'80%'
+    },
+    inputBoxText:{
+        fontWeight:'bold',
+        alignSelf:'flex-start',
+        color:'white'
+    },
+    textInputStyle:{
+        borderBottomWidth:0.5,
+        borderColor:'white',
+        width:'100%',
+        height:50,
+        borderRadius:10,
+        marginVertical:10,
+        textAlign:'center',
+      }
+ })
+
+
+
