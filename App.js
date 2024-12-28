80e634f0fa63a899483e1c3cadf62e8474f23132
Dilay Kal <104075669+dilaykal@users.noreply.github.com>
2024-12-28 17:35:26 +0300

    app.js sadeleştirildi

--- App.js
+++ App.js
@@ -1,68 +1,94 @@
+
+//import React, { useEffect } from 'react';
+// import { StatusBar } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import { db } from './firebaseConfig';
+// import { collection, getDocs } from 'firebase/firestore';
+// import { 
+//  HomeScreen,
+//  LoginScreen,
+//  RegisterScreen,
+//  AdminLoginScreen,
+//  AdminDashboard 
+// } from './screens';
+
+
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { StatusBar } from 'react-native';
-import { 
- HomeScreen,
- LoginScreen,
- RegisterScreen,
- AdminLoginScreen,
- AdminDashboard 
-} from './screens';
+import RootNavigation from './navigation/rootNavigation';
 
-const Stack = createStackNavigator();
+const App = () =>{
+  return (<RootNavigation/>);
 
-const App = () => {
- return (
-   <NavigationContainer>
-     <StatusBar
-       barStyle="light-content"
-       backgroundColor="#007AFF"
-       translucent={true}
-     />
-     <Stack.Navigator
-       initialRouteName="Home"
-       screenOptions={{
-         headerStyle: {
-           backgroundColor: '#007AFF',
-         },
-         headerTintColor: '#fff',
-         headerTitleStyle: {
-           fontWeight: 'bold',
-         },
-       }}
-     >
-       <Stack.Screen
-         name="Home"
-         component={HomeScreen}
-         options={{ title: 'Ana Sayfa' }}
-       />
-       <Stack.Screen
-         name="Login"
-         component={LoginScreen}
-         options={{ title: 'Kullanıcı Girişi' }}
-       />
-       <Stack.Screen
-         name="Register"
-         component={RegisterScreen}
-         options={{ title: 'Kayıt Ol' }}
-       />
-       <Stack.Screen
-         name="AdminLogin"
-         component={AdminLoginScreen}
-         options={{ title: 'Yönetici Girişi' }}
-       />
-       <Stack.Screen
-         name="AdminDashboard"
-         component={AdminDashboard}
-         options={{
-           title: 'Yönetici Paneli',
-           headerLeft: null,
-         }}
-       />
-     </Stack.Navigator>
-   </NavigationContainer>
- );
 };
 
 export default App;
+
+
+
+
+
+// const Stack = createStackNavigator();
+
+// const App = () => {
+//   useEffect(() => {
+//     async function fetchData() {
+//       const querySnapshot = await getDocs(collection(db, "TahlilTakipSistemi"));
+//       console.log("Data:", querySnapshot.docs.map(doc => doc.data()));
+//     }
+//     fetchData();
+//   }, []);
+//  return (
+//    <NavigationContainer>
+//      <StatusBar
+//        barStyle="light-content"
+//        backgroundColor="#007AFF"
+//        translucent={true}
+//      />
+//      <Stack.Navigator
+//        initialRouteName="Home"
+//        screenOptions={{
+//          headerStyle: {
+//            backgroundColor: '#007AFF',
+//          },
+//          headerTintColor: '#fff',
+//          headerShown:false,
+//          headerTitleStyle: {
+//            fontWeight: 'bold',
+//          },
+//        }}
+//      >
+//        <Stack.Screen
+//          name="Home"
+//          component={HomeScreen}
+//          options={{ title: 'Ana Sayfa' }}
+//        />
+//        <Stack.Screen
+//          name="Login"
+//          component={LoginScreen}
+//          options={{ title: 'Kullanıcı Girişi' }}
+//        />
+//        <Stack.Screen
+//          name="Register"
+//          component={RegisterScreen}
+//          options={{ title: 'Kayıt Ol' }}
+//        />
+//        <Stack.Screen
+//          name="AdminLogin"
+//          component={AdminLoginScreen}
+//          options={{ title: 'Yönetici Girişi' }}
+//        />
+//        <Stack.Screen
+//          name="AdminDashboard"
+//          component={AdminDashboard}
+//          options={{
+//            title: 'Yönetici Paneli',
+//            headerLeft: null,
+//          }}
+//        />
+//      </Stack.Navigator>
+//    </NavigationContainer>
+//  );
+// };
+
+// export default App;
